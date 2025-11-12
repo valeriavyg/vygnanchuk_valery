bb10f9d1aca97693aed108e97b610fb269fcf2e3
Valeria <vygnanchuk.vs@phystech.edu>
2025-11-12 21:16:34 +0300

    Update 03.cpp

--- seminar7_ref_string_vector/03.cpp
+++ seminar7_ref_string_vector/03.cpp
@@ -9,9 +9,10 @@ void swap(int& a,int& b)
 
 int main()
 {
-int a = 10;
-int b = 20;
-std::cout << a << " " << b << std::endl; // Должно напечатать 10 20
-swap(a, b);
-std::cout << a << " " << b << std::endl; // Должно напечатать 20 10
+    int a = 10;
+    int b = 20;
+    std::cout << a << " " << b << std::endl; // Должно напечатать 10 20
+    swap(a, b);
+    std::cout << a << " " << b << std::endl; // Должно напечатать 20 10
 }
+
