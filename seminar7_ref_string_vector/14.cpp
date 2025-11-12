e0b19901263a1d9e01ae6c53349eedef8488bf60
Valeria <vygnanchuk.vs@phystech.edu>
2025-11-12 21:14:34 +0300

    Update 14.cpp

--- seminar7_ref_string_vector/14.cpp
+++ seminar7_ref_string_vector/14.cpp
@@ -22,12 +22,13 @@ std::vector<std::size_t> substringIndexes(const std::string& str, const std::str
 }
 int main()
 {
-std::vector<std::size_t> v1 = substringIndexes("cat and dog and cat", "cat");
-print(v1); // Должно напечатать 0 16
-std::vector<std::size_t> v2 = substringIndexes("look, cats were here", "cat");
-print(v2); // Должно напечатать 6
-std::vector<std::size_t> v3 = substringIndexes("catcatcatcatcatcat", "cat");
-print(v3); // Должно напечатать 0 3 6 9 12 15
-std::vector<std::size_t> v4 = substringIndexes("dog mouse elephant", "cat");
-print(v4); // Не должно ничего печатать
+    std::vector<std::size_t> v1 = substringIndexes("cat and dog and cat", "cat");
+    print(v1); // Должно напечатать 0 16
+    std::vector<std::size_t> v2 = substringIndexes("look, cats were here", "cat");
+    print(v2); // Должно напечатать 6
+    std::vector<std::size_t> v3 = substringIndexes("catcatcatcatcatcat", "cat");
+    print(v3); // Должно напечатать 0 3 6 9 12 15
+    std::vector<std::size_t> v4 = substringIndexes("dog mouse elephant", "cat");
+    print(v4); // Не должно ничего печатать
+
 }
